db81e18844bc377f50eeb9e3721a42305b75ea6e
Brambilla05 <brambillagianluca@itis-molinari.eu>
2025-02-02 18:24:46 +0100

    fine

--- index.js
+++ index.js
@@ -20,116 +20,111 @@ app.use(
 );
 app.use("/", express.static(path.join(__dirname, "public")));
 
-app.post("/todo/add", (req, res) => {
+app.post("/todo/add", async (req, res) => {
   const todo = req.body.todo;
 
-  insert(todo);
+  await insert(todo);
 
   res.json({ result: "Ok" });
 });
 
-app.get("/todo", (req, res) => {
-  select().then(todos => res.json({ todos: todos }));
+app.get("/todo", async (req, res) => {
+  const todo = await select()
+  res.json({ todos: todo });
 });
 
-app.put("/todo/complete", (req, res) => {
+app.put("/todo/complete", async (req, res) => {
   const todo = req.body;
 
   try {
-    select()
-    .then(todos => {
-      todos.map((element) => {
+    let todos = await select()
+
+    await todos.map(async (element) => {
       if (element.id === todo.id) {
         element.completed = true;
-        update(element);
+        await update(element);
       }
-
       return element;
     });
-    })
-    
+
   } catch (e) {
     console.log(e);
   }
   res.json({ result: "Ok" });
 });
 
-app.delete("/todo/:id", (req, res) => {
-  select()
-  .then(todos => {
-    todos = todos.filter((element) => element.id == req.params.id);
-    remove(todos[0]);
-  })
-  
+app.delete("/todo/:id", async (req, res) => {
+  let todos = await select()
+  todos = todos.filter((element) => element.id == req.params.id);
+  await remove(todos[0]);
+
   res.json({ result: "Ok" });
 });
 
-
-
 const executeQuery = (sql) => {
-    return new Promise((resolve, reject) => {      
-          connection.query(sql, function (err, result) {
-             if (err) {
-                console.error(err);
-                reject();     
-             }   
-             console.log('done');
-             resolve(result);         
-       });
-    })
- }
+  return new Promise((resolve, reject) => {
+    connection.query(sql, function (err, result) {
+      if (err) {
+        console.error(err);
+        reject();
+      }
+      console.log('done');
+      resolve(result);
+    });
+  })
+}
 
- const createTable = () => {
-    return executeQuery(`
+const createTable = async () => {
+  return await executeQuery(`
     CREATE TABLE IF NOT EXISTS todo
        ( id INT PRIMARY KEY AUTO_INCREMENT, 
           name VARCHAR(255) NOT NULL, 
           completed BOOLEAN ) 
-       `);      
- }
+       `);
+}
 
- const insert = (todo) => {
-    const template = `
+const insert = async (todo) => {
+  const template = `
     INSERT INTO todo (name, completed) VALUES ('$NAME', '$COMPLETED')
        `;
-    let sql = template.replace("$NAME", todo.name);
-    sql = sql.replace("$COMPLETED", todo.completed ? 1 : 0);
-    return executeQuery(sql); 
- }
+  let sql = template.replace("$NAME", todo.name);
+  sql = sql.replace("$COMPLETED", todo.completed ? 1 : 0);
+  return await executeQuery(sql);
+}
 
- const select = () => {
-    const sql = `
+const select = async () => {
+  const sql = `
     SELECT id, name, completed FROM todo 
        `;
-    return executeQuery(sql); 
- }
+  return await executeQuery(sql);
+}
 
- const update = (todo) =>{
-    let sql = `
+const update = async (todo) => {
+  let sql = `
     UPDATE todo SET completed = '$COMPLETED' WHERE id = '$ID'
     `;
 
-    sql = sql.replace("$COMPLETED", todo.completed  ? 1 : 0);
-    sql = sql.replace("$ID", todo.id);
-    return executeQuery(sql); 
- }
+  sql = sql.replace("$COMPLETED", todo.completed ? 1 : 0);
+  sql = sql.replace("$ID", todo.id);
+  return await executeQuery(sql);
+}
 
 
- const remove = (todo) => {
-  let sql =  `
+const remove = async (todo) => {
+  let sql = `
   DELETE FROM todo WHERE id = '$ID'
   `;
 
   sql = sql.replace("$ID", todo.id);
-  return executeQuery(sql); 
- }
+  return await executeQuery(sql);
+}
 
- 
- createTable().then ( () => {
+
+createTable().then(() => {
   const server = http.createServer(app);
 
   server.listen(80, () => {
     console.log("- server running");
   })
-  }
+}
 );
--- public/index.js
+++ public/index.js
@@ -36,158 +36,86 @@ const render = () => {
 }
 
 const send = (todo) => {
-
     return new Promise((resolve, reject) => {
-
         fetch("/todo/add", {
-
             method: 'POST',
-
             headers: {
-
                 "Content-Type": "application/json"
-
             },
-
             body: JSON.stringify(todo)
-
         })
-
             .then((response) => response.json())
-
             .then((json) => {
-
                 resolve(json);
-
             })
-
     })
-
 }
 
 const load = () => {
-
     return new Promise((resolve, reject) => {
-
         fetch("/todo")
-
             .then((response) => response.json())
-
             .then((json) => {
-
                 resolve(json);
-
             })
-
     })
-
 }
 
 insertButton.onclick = () => {
-
     const todo = {
-
         name: todoInput.value,
-
         completed: false
-
     }
-
     send({ todo: todo })
-
         .then(() => load())
-
         .then((json) => {
-
             todos = json.todos;
-
             todoInput.value = "";
-
             render();
-
         });
-
 }
 
 load().then((json) => {
-
     todos = json.todos;
-
     render();
-
 });
 
-
 const completeTodo = (todo) => {
-
     return new Promise((resolve, reject) => {
-
         fetch("/todo/complete", {
-
             method: 'PUT',
-
             headers: {
-
                 "Content-Type": "application/json"
-
             },
-
             body: JSON.stringify(todo)
-
         })
-
             .then((response) => response.json())
-
             .then((json) => {
-
                 resolve(json);
-
             })
-
     })
-
 }
-
 
 const deleteTodo = (id) => {
-
     return new Promise((resolve, reject) => {
-
         fetch("/todo/" + id, {
-
             method: 'DELETE',
-
             headers: {
-
                 "Content-Type": "application/json"
 
             },
-
         })
-
             .then((response) => response.json())
-
             .then((json) => {
-
                 resolve(json);
-
             })
-
     })
-
 }
 
-
 setInterval(() => {
-
     load().then((json) => {
-
         todos = json.todos;
-
         todoInput.value = "";
-
         render();
-
     });
-
 }, 30000);
